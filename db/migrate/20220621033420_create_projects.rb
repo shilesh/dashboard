d4d2a2ba8996eb0173720e7d142573852a18478b
Shilesh Kumar S K <shilesh.kumar@v2solutions.com>
2022-06-30 16:22:15 +0530

    Correcting migration

--- db/migrate/20220621033420_create_projects.rb
+++ db/migrate/20220621033420_create_projects.rb
@@ -1,7 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.integer :id
       t.string :title
       t.string :contact_email
 
--- db/migrate/20220621033458_create_users.rb
+++ db/migrate/20220621033458_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :email
       t.string :uuid
       t.string :type
